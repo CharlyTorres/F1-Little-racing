807c2bb818d80d37f98ace221d3e84e655729fd5
CHarly <cgtright@gmail.com>
2020-01-23 00:06:50 -0600

    Juego terminado, solo falta lo fancy

--- script.js
+++ script.js
@@ -30,9 +30,9 @@ class SpeedWay{
       this.height = canvas.height
       this.img = new Image()
       this.img.src = images.bg
-      this.img.onload = () => {
+      /* this.img.onload = () => {
         this.draw()
-      }
+      } */
     }
     draw() {
      // this.x--
@@ -66,6 +66,22 @@ class Car1 {
    
    rotateRight(){
      if(this.x >= 1060) return
+
+     if(this.x < limit9.x + limit9.width &&
+      this.x + this.width > limit9.x &&
+      this.y < limit9.y + limit9.height &&
+      this.height + this.y > limit9.y) return
+
+    if(this.x < limit11.x + limit11.width &&
+      this.x + this.width > limit11.x &&
+      this.y < limit11.y + limit11.height &&
+      this.height + this.y > limit11.y) return
+
+    if(this.x < limit15.x + limit15.width &&
+      this.x + this.width > limit15.x &&
+      this.y < limit15.y + limit15.height &&
+      this.height + this.y > limit15.y) return
+
      this.x += 10
      this.car.src = images.mercedes
      this.width = 60
@@ -73,6 +89,32 @@ class Car1 {
     } 
     rotateLeft(){
       if(this.x <= 20) return
+
+      if(this.x < limit8.x + limit8.width &&
+        this.x + this.width > limit8.x &&
+        this.y < limit8.y + limit8.height &&
+        this.height + this.y > limit8.y) return
+        
+      if(this.x < limit10.x + limit10.width &&
+        this.x + this.width > limit10.x &&
+        this.y < limit10.y + limit10.height &&
+        this.height + this.y > limit10.y) return
+
+      if(this.x < limit12.x + limit12.width &&
+        this.x + this.width > limit12.x &&
+        this.y < limit12.y + limit12.height &&
+        this.height + this.y > limit12.y) return
+
+      if(this.x < limit13.x + limit13.width &&
+        this.x + this.width > limit13.x &&
+        this.y < limit13.y + limit13.height &&
+        this.height + this.y > limit13.y) return
+
+      if(this.x < limit14.x + limit14.width &&
+        this.x + this.width > limit14.x &&
+        this.y < limit14.y + limit14.height &&
+        this.height + this.y > limit14.y) return
+
       this.x -= 10
       this.car.src = images.mercedesleft
       this.width = 60
@@ -80,10 +122,27 @@ class Car1 {
     }
     rotateDown(){
       if(this.y >= 500) return
+
       if(this.x < limit1.x + limit1.width &&
         this.x + this.width > limit1.x &&
         this.y < limit1.y + limit1.height &&
         this.height + this.y > limit1.y) return
+
+      if(this.x < limit4.x + limit4.width &&
+        this.x + this.width > limit4.x &&
+        this.y < limit4.y + limit4.height &&
+        this.height + this.y > limit4.y) return
+
+      if(this.x < limit6.x + limit6.width &&
+        this.x + this.width > limit6.x &&
+        this.y < limit6.y + limit6.height &&
+        this.height + this.y > limit6.y) return
+
+      if(this.x < limit7.x + limit7.width &&
+        this.x + this.width > limit7.x &&
+        this.y < limit7.y + limit7.height &&
+        this.height + this.y > limit7.y) return
+
       this.y += 10
       this.car.src = images.mercedesdown
       this.width = 25
@@ -91,6 +150,22 @@ class Car1 {
     }
     rotateUp(){
       if(this.y <= 0) return
+
+      if(this.x < limit2.x + limit2.width &&
+        this.x + this.width > limit2.x &&
+        this.y < limit2.y + limit2.height &&
+        this.height + this.y > limit2.y) return
+
+      if(this.x < limit3.x + limit3.width &&
+        this.x + this.width > limit3.x &&
+        this.y < limit3.y + limit3.height &&
+        this.height + this.y > limit3.y) return
+
+      if(this.x < limit5.x + limit5.width &&
+        this.x + this.width > limit5.x &&
+        this.y < limit5.y + limit5.height &&
+        this.height + this.y > limit5.y) return
+
       this.y -= 10
       this.car.src = images.mercedesup
       this.width = 25
@@ -130,9 +205,6 @@ class Car2 {
       this.height = 25
       this.car = new Image()
       this.car.src = images.ferrari
-  //    this.car.onload = () => {
-    //    this.draw()
-    //}
 }
   
     draw(){
@@ -141,6 +213,22 @@ class Car2 {
   
     goRight(){
       if(this.x >= 1060) return
+
+      if(this.x < limit9.x + limit9.width &&
+        this.x + this.width > limit9.x &&
+        this.y < limit9.y + limit9.height &&
+        this.height + this.y > limit9.y) return
+  
+      if(this.x < limit11.x + limit11.width &&
+        this.x + this.width > limit11.x &&
+        this.y < limit11.y + limit11.height &&
+        this.height + this.y > limit11.y) return
+  
+      if(this.x < limit15.x + limit15.width &&
+        this.x + this.width > limit15.x &&
+        this.y < limit15.y + limit15.height &&
+        this.height + this.y > limit15.y) return
+
       this.car.src = images.ferrari
       this.width = 60
       this.height = 25
@@ -149,6 +237,32 @@ class Car2 {
     
     goLeft(){
       if(this.x <= 20) return
+
+      if(this.x < limit8.x + limit8.width &&
+        this.x + this.width > limit8.x &&
+        this.y < limit8.y + limit8.height &&
+        this.height + this.y > limit8.y) return
+        
+      if(this.x < limit10.x + limit10.width &&
+        this.x + this.width > limit10.x &&
+        this.y < limit10.y + limit10.height &&
+        this.height + this.y > limit10.y) return
+
+      if(this.x < limit12.x + limit12.width &&
+        this.x + this.width > limit12.x &&
+        this.y < limit12.y + limit12.height &&
+        this.height + this.y > limit12.y) return
+
+      if(this.x < limit13.x + limit13.width &&
+        this.x + this.width > limit13.x &&
+        this.y < limit13.y + limit13.height &&
+        this.height + this.y > limit13.y) return
+
+      if(this.x < limit14.x + limit14.width &&
+        this.x + this.width > limit14.x &&
+        this.y < limit14.y + limit14.height &&
+        this.height + this.y > limit14.y) return
+
       this.car.src = images.ferrarileft
       this.width = 60
       this.height = 25
@@ -156,6 +270,22 @@ class Car2 {
     }
     goUp(){
       if(this.y <= 0) return
+
+      if(this.x < limit2.x + limit2.width &&
+        this.x + this.width > limit2.x &&
+        this.y < limit2.y + limit2.height &&
+        this.height + this.y > limit2.y) return
+
+      if(this.x < limit3.x + limit3.width &&
+        this.x + this.width > limit3.x &&
+        this.y < limit3.y + limit3.height &&
+        this.height + this.y > limit3.y) return
+
+      if(this.x < limit5.x + limit5.width &&
+        this.x + this.width > limit5.x &&
+        this.y < limit5.y + limit5.height &&
+        this.height + this.y > limit5.y) return
+
       this.y -= 10
       this.car.src = images.ferrariup
       this.width = 25
@@ -163,10 +293,39 @@ class Car2 {
     }
     goDown(){
       if(this.y >= 500) return
+
+      if(this.x < limit1.x + limit1.width &&
+        this.x + this.width > limit1.x &&
+        this.y < limit1.y + limit1.height &&
+        this.height + this.y > limit1.y) return
+
+      if(this.x < limit4.x + limit4.width &&
+        this.x + this.width > limit4.x &&
+        this.y < limit4.y + limit4.height &&
+        this.height + this.y > limit4.y) return
+
+      if(this.x < limit6.x + limit6.width &&
+        this.x + this.width > limit6.x &&
+        this.y < limit6.y + limit6.height &&
+        this.height + this.y > limit6.y) return
+
+      if(this.x < limit7.x + limit7.width &&
+        this.x + this.width > limit7.x &&
+        this.y < limit7.y + limit7.height &&
+        this.height + this.y > limit7.y) return
+
       this.y += 10
       this.car.src = images.ferraridown
       this.width = 25
       this.height = 60
+    }
+    lapScore(){
+      return (
+        this.x < lapup.x + lapup.width &&
+        this.x + this.width > lapup.x &&
+        this.y < lapup.y + lapup.height &&
+        this.height + this.y > lapup.y
+      )
     }
   }
 
@@ -196,25 +355,256 @@ class Car2 {
     this.height = 1
     }
     draw(){
-      ctx.fillStyle = "black"
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
       ctx.fillRect(this.x, this.y, this.width, this.height)
     }
   }
 
   let limit1 = new Limit1()
 
+  class Limit2{
+    constructor(){
+    this.x = 130
+    this.y = 439
+    this.width = 820
+    this.height = 1
+    }
+    draw(){
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  let limit2 = new Limit2()
+
+  class Limit3{
+    constructor(){
+    this.x = 250
+    this.y = 261
+    this.width = 830
+    this.height = 1
+    }
+    draw(){
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  let limit3 = new Limit3()
+
+  class Limit4{
+    constructor(){
+    this.x = 240
+    this.y = 162
+    this.width = 600
+    this.height = 1
+    }
+    draw(){
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  let limit4 = new Limit4()
+
+  class Limit5{
+    constructor(){
+    this.x = 130
+    this.y = 90
+    this.width = 790
+    this.height = 1
+    }
+    draw(){
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  let limit5 = new Limit5()
+
+  class Limit6{
+    constructor(){
+    this.x = 130
+    this.y = 78
+    this.width = 830
+    this.height = 1
+    }
+    draw(){
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  let limit6 = new Limit6()
+
+  class Limit7{
+    constructor(){
+    this.x = 845
+    this.y = 252
+    this.width = 245
+    this.height = 1
+    }
+    draw(){
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  let limit7 = new Limit7()
+
+  class Limit8{
+    constructor(){
+    this.x = 841
+    this.y = 159
+    this.width = 1
+    this.height = 100
+    }
+    draw(){
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  let limit8 = new Limit8()
+
+  class Limit9{
+    constructor(){
+    this.x = 235
+    this.y = 165
+    this.width = 1
+    this.height = 95
+    }
+    draw(){
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  let limit9 = new Limit9()
+
+  class Limit10{
+    constructor(){
+    this.x = 125
+    this.y = 100
+    this.width = 1
+    this.height = 240
+    }
+    draw(){
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  let limit10 = new Limit10()
+
+  class Limit11{
+    constructor(){
+    this.x = 96
+    this.y = 100
+    this.width = 1
+    this.height = 320
+    }
+    draw(){
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  let limit11 = new Limit11()
+
+  class Limit12{
+    constructor(){
+    this.x = 300
+    this.y = 400
+    this.width = 1
+    this.height = 320
+    }
+    draw(){
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  let limit12 = new Limit12()
+
+  class Limit13{
+    constructor(){
+    this.x = 980
+    this.y = 350
+    this.width = 1
+    this.height = 80
+    }
+    draw(){
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  let limit13 = new Limit13()
+
+  class Limit14{
+    constructor(){
+    this.x = 985
+    this.y = 90
+    this.width = 1
+    this.height = 80
+    }
+    draw(){
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  let limit14 = new Limit14()
+
+  class Limit15{
+    constructor(){
+    this.x = 930
+    this.y = 90
+    this.width = 1
+    this.height = 80
+    }
+    draw(){
+      ctx.fillStyle = "rgba(0, 0, 0, 0)"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  let limit15 = new Limit15()
+
   function checkLaps() {
 
           if(firstCar.lapScore()) {
             lap +=1 
-          }
-            //lapBool2 = true
-          /* }
+            if (lap>1){
+              lap = 1
+            }
+            if (firstCar.lapScore() && lap === 1){
+              ctx.clearRect(0, 0, canvas.width, canvas.height)
+              ctx.font = '100px serif'
+              ctx.fillStyle = 'green'
+              ctx.fillText(String('PUTOS TODOS'), 400, 500)
+              clearInterval(interval)
 
-          if(lapBool2 && lapBool2){
-            console.log(lap)
-            return lap += 1
-          } */
+
+            }
+          }
+
+          if(secondCar.lapScore()) {
+            lap +=1 
+            if (lap>1){
+              lap = 1
+            }
+            if (secondCar.lapScore() && lap === 1){
+              ctx.clearRect(0, 0, canvas.width, canvas.height)
+              ctx.font = '100px serif'
+              ctx.fillStyle = 'green'
+              ctx.fillText(String('PUTAS TODAS'), 400, 500)
+              clearInterval(interval)
+
+
+            }
+          }
          
         
       
@@ -225,6 +615,7 @@ class Car2 {
     interval = setInterval(update, 1000 / 60)
   }
 
+
 function update() {
     frames++
     ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -233,10 +624,27 @@ function update() {
     secondCar.draw()
     lapup.draw()
     limit1.draw()
-    ctx.fillText(String(lap), canvas.width - 100, 100)
+    limit2.draw()
+    limit3.draw()
+    limit4.draw()
+    limit5.draw()
+    limit6.draw()
+    limit7.draw()
+    limit8.draw()
+    limit9.draw()
+    limit10.draw()
+    limit11.draw()
+    limit12.draw()
+    limit13.draw()
+    limit14.draw()
+    limit15.draw()
     checkLaps()
 
   }
+
+  function URL() {
+    location.href = 'index.html';
+}
 
   document.querySelector('.start').onclick = () => {
       startGame()
@@ -257,6 +665,15 @@ function update() {
      
     function keysPressed(e) {
         keys[e.keyCode] = true
+        
+        if (keys[82]) {
+          URL()
+        }
+
+        if (keys[13]) {
+          startGame()
+        }
+
         //Car 1
         if (keys[39] && keys[38]) {
           firstCar.diagonalUpRight();
@@ -289,24 +706,6 @@ function update() {
         if (keys[40]) {
           firstCar.rotateDown()
         }
-
-        //Car2
-        /* if (keys[39] && keys[38]) {
-          firstCar.diagonalUpRight();
-        }
-
-        if (keys[39] && keys[40]) {
-          firstCar.diagonalDownRight();
-        }
-
-        if (keys[37] && keys[40]) {
-          firstCar.diagonalDownLeft();
-        }
-
-        if (keys[38] && keys[37]) {
-          firstCar.diagonalUpLeft();
-        } */
-
         if (keys[68]) {
           secondCar.goRight()
         }
